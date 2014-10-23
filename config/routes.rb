5f26f6e45d8c64f7581e09b2ea6b8099bde1b2c7
Anthony Elliott <ajellio2@ncsu.edu>
2014-10-23 15:08:10 -0400

    Routes to create group

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+   resources :groups
 
   # Example resource route with options:
   #   resources :products do
