92b8e298223141e43d4d28ee1d9721ef796ec153
James Beavers <jamesjbeavers@gmail.com>
2014-11-08 22:03:19 -0500

    Add name field for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # Taken from http://stackoverflow.com/questions/16297797/add-custom-field-column-to-devise-with-rails-4
+  def configure_devise_permitted_parameters
+    registration_params = [:name, :email, :password, :password_confirmation]
+
+    if params[:action] == 'update'
+      devise_parameter_sanitizer.for(:account_update) {
+          |u| u.permit(registration_params << :current_password)
+      }
+    elsif params[:action] == 'create'
+      devise_parameter_sanitizer.for(:sign_up) {
+          |u| u.permit(registration_params)
+      }
+    end
+  end
 end
