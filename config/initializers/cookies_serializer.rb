d7072f74bb8113405bd15e700a404a8a7aa0888e
James Beavers <jamesjbeavers@gmail.com>
2014-11-08 20:23:10 -0500

    Add user views and add authentication

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.action_dispatch.cookies_serializer = :json
+# Comment out due to Mongo/mongoid/devise issue - see https://github.com/plataformatec/devise/issues/2949
+#Rails.application.config.action_dispatch.cookies_serializer = :json
