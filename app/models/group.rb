a9222960f3e9c202bbe03134e2a73e8f412f3cb1
James Beavers <jamesjbeavers@gmail.com>
2014-11-17 22:58:44 -0500

    Add many-many relationship and store leader_id in group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,8 @@
 class Group
   include Mongoid::Document
+  has_and_belongs_to_many :users
 
-  field :leader, type: String
+  field :leader_id
   field :course, type: String
   field :participantNumber, type: Integer, default: 1
   field :frequency, type: String
@@ -9,5 +10,9 @@ class Group
   field :time, type: Time
   field :dayOfWeek, type: String
   field :date, type: Date
+
+  def leader
+    User.find(leader_id)
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User
   include Mongoid::Document
+  has_and_belongs_to_many :groups
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
