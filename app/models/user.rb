dab0e788a953be3a4eb04207bb7d257ca6997967
James Beavers <jamesjbeavers@gmail.com>
2014-11-24 19:31:40 -0500

    Hide apply button from users who have already submitted an application. Added groups (back) button

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User
   include Mongoid::Document
   has_and_belongs_to_many :groups
+  has_many :group_applications
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -32,5 +34,9 @@ class User
 
   def is_member group
     group.users.include? self
+  end
+
+  def is_pending_applicant? group
+    self.group_applications.include?(group)
   end
 end
