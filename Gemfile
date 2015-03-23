967de060a87f5445b7b3229ba67def028c79ca48
James Beavers <jamesjbeavers@gmail.com>
2015-03-22 20:53:27 -0400

    Remove google sign on, and additional gemfile cleanup

--- Gemfile
+++ Gemfile
@@ -21,29 +21,12 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0',          group: :doc
 
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
-
-# Use debugger
-# gem 'debugger', group: [:development, :test]
-
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin]
 
 # User authentication
 gem 'devise'
 gem 'therubyracer'
-# For oauth authentication with google
-gem 'omniauth'
-gem 'omniauth-google-oauth2'
-# For communicating with google calendar api
-gem 'google-api-client'
 
 # Use Bootstrap
 #gem "therubyracer"
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -232,14 +232,14 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  require 'omniauth-google-oauth2'
-  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
-      :scope => "email, profile, https://www.googleapis.com/auth/calendar",
-      :prompt => "select_account",
-      :image_aspect_ratio => "square",
-      :image_size => 50,
-      :access_type => "offline"
-    }
+  # require 'omniauth-google-oauth2'
+  # config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
+  #     :scope => "email, profile, https://www.googleapis.com/auth/calendar",
+  #     :prompt => "select_account",
+  #     :image_aspect_ratio => "square",
+  #     :image_size => 50,
+  #     :access_type => "offline"
+  #   }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
