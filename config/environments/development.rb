fd841b43eb5759789fdfc30a531d99a76560dd70
James Beavers <jamesjbeavers@gmail.com>
2015-04-13 14:49:44 -0400

    Add mailer config for password reset -- environment variables need to be set to enable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,16 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: ENV["GMAIL_DOMAIN"],
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+}
+
 end
